2d9bb32d73c8879e7325fb07a4b59224bbb4694b
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-03-27 15:11:33 -0300

    Add database config, middleware and route to the backend entry point

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
+connectDB();
+
 const app = express();
+
+app.use('/api/users', userRoutes);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
