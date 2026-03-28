0e0ee955af4461625189ad134c93d506612fc482
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-03-28 11:24:49 -0300

    Attach protect middleware into get and update profile

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
@@ -13,6 +14,9 @@ const router = express.Router();
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
