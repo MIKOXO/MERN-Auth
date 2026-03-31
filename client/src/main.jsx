87e0dead4b1e16176a2170417f670e69bca557c0
Mikiyas Nigatu <mikermount19@yahoo.com>
2026-03-31 06:21:10 -0300

    Add auth page routes and redux provider wrapper

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,34 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './store.js';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
+import Homepage from './pages/Homepage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import RegisterPage from './pages/RegisterPage.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<Homepage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>,
 );
